327fa375962a12c6d4e5143f796b65fcc2004062
admin01 <paradoxraphael@gmai.com>
2024-11-03 13:59:28 +1100

    for the desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 function App() {
   return (
     <>
-      <div className="bg-slate-900 text-red-600 ">hello</div>
+      <div className="bg-slate-300 h-dvh flex items-center justify-center">
+        <div className="flex flex-col bg-white rounded-2xl w-72 p-4 shadow-md gap-4">
+          <img className="rounded-xl" src="image-qr-code.png" />
+          <p className="text-slate-900 font-bold text-xl text-center leading-6">
+            Improve your font-end skills by building projects
+          </p>
+          <p className="text-slate-500 text-sm text-center px-2 pb-4">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
     </>
   );
 }
